8813639bbd5e41e8d36cb6ed6fa85a26a1606743
Lina Chihoub <lchihoub@sas.upenn.edu>
2024-04-21 11:01:44 -0400

    Initial Commit

---
+++ FinalProj/FinalProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjApp.swift
+//  FinalProj
+//
+//  Created by Lina Chihoub on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinalProj/Models/Emotions.swift
@@ -0,0 +1,18 @@
+//
+//  Emotions.swift
+//  FinalProj
+//
+//  Created by Lina Chihoub on 4/21/24.
+//
+
+import Foundation
+
+
+struct Emotion  {
+    var name: String // the type of emotion, e.g. happiness
+    var emoji: String // emoji representing the image for the emotion 
+    var severity: Double // severity of emotion
+    var reflection: String // the reflection of why you feel that way for the emotion
+}
+
+
