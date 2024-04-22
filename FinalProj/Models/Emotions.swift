6883974b0aa547cff388442f0b247b4681f65bc9
YXK031219 <137147208+YXK031219@users.noreply.github.com>
2024-04-22 18:50:18 -0400

    Update Emotions.swift

--- FinalProj/Models/Emotions.swift
+++ FinalProj/Models/Emotions.swift
@@ -8,11 +8,9 @@
 import Foundation
 
 
-struct Emotion  {
+struct Emotion: Codable  {
     var name: String // the type of emotion, e.g. happiness
     var emoji: String // emoji representing the image for the emotion 
     var severity: Double // severity of emotion
     var reflection: String // the reflection of why you feel that way for the emotion
 }
-
-
